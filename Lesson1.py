55f86733930111b0d5d24a50420738806fd902ce
rythmeur <rythmeur@gmail.com>
2014-01-03 01:56:31 +0400

    first try

--- Lesson1.py
+++ Lesson1.py
@@ -1,8 +1,9 @@
+# This Python file uses the following encoding: utf-8
 z = ["ab", 'ds']
-for r,s in z:
+for r,s in z: # iterable можно разбивать на переменные
     print r,s
 
 k = None
-z = k or 28
+z = k or 28 # прикольно - если None то приравнивается другое значение
 print z
 
