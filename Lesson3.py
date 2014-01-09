b81bc6911362816e05afda1b2bb4571dd4de818f
rythmeur <rythmeur@gmail.com>
2014-01-10 01:09:18 +0400

    10-01-2014

--- Lesson3.py
+++ Lesson3.py
@@ -115,18 +115,57 @@ def matchset(pattern, text):
         return matchset(x, text) | matchset(y, text)
 '''
 
-def lit(s): return lambda text: set([text[len(s):]]) if text.startswith(s) else null
+# def lit(s): return lambda text: set([text[len(s):]]) if text.startswith(s) else null
+#
+# def seq(x, y): return lambda text: set().union(*map(y, x(text)))
+#
+# def alt(x, y):return lambda text: set().union(x(text) ,y(text)) # your code here
+#
+# null = frozenset([])
+#
+# def test():
+#     g = alt(lit('a'), lit('b'))
+#     print g('abc')
+#     # assert g('abc') == set(['bc'])
+#     return 'test passes'
+#
+# print test()
 
-def seq(x, y): return lambda text: set().union(*map(y, x(text)))
+# --------------
+# User Instructions
+#
+# Complete the code for the compiler by completing the constructor
+# for the patterns alt(x, y) and oneof(chars).
 
-def alt(x, y):return lambda text: set().union(x(text) ,y(text)) # your code here
+def lit(s):         return lambda Ns: set([s]) if len(s) in Ns else null
+def alt(x, y):      return lambda Ns:  x(Ns)| y(Ns)# your code here
+def star(x):        return lambda Ns: opt(plus(x))(Ns)
+def plus(x):        return lambda Ns: genseq(x, star(x), Ns, startx=1) #Tricky
+def oneof(chars):   return lambda Ns: set([ch for ch in chars]) if (sum (Ns)) == len (set([ch for ch in chars]) ) else null # set('theseletters') - without any loop
+def seq(x, y):      return lambda Ns: genseq(x, y, Ns)
+def opt(x):         return alt(epsilon, x)
+dot = oneof('?')    # You could expand the alphabet to more chars.
+epsilon = lit('')   # The pattern that matches the empty string.
 
 null = frozenset([])
 
 def test():
-    g = alt(lit('a'), lit('b'))
-    print g('abc')
-    # assert g('abc') == set(['bc'])
-    return 'test passes'
 
+    f = lit('hello')
+    assert f(set([1, 2, 3, 4, 5])) == set(['hello'])
+    assert f(set([1, 2, 3, 4]))    == null
+
+    g = alt(lit('hi'), lit('bye'))
+    assert g(set([1, 2, 3, 4, 5, 6])) == set(['bye', 'hi'])
+    assert g(set([1, 3, 5])) == set(['bye'])
+
+    h = oneof('theseletters')
+    assert h(set([1, 2, 3])) == set(['t', 'h', 'e', 's', 'l', 'r'])
+    assert h(set([2, 3, 4])) == null
+
+    return 'tests pass'
+
+
+h = oneof('theseletters')
+print  h(set([1, 2, 3]))
 print test()
--- Test/123.py
+++ Test/123.py
@@ -1,10 +1,16 @@
-print  [a for a in [1, 2] ]
-houses = first, _, middle, _, _ = [1, 2, 3, 4, 5]
-print houses
-print first
+# print  [a for a in [1, 2] ]
+# houses = first, _, middle, _, _ = [1, 2, 3, 4, 5]
+# print houses
+# print first
+#
+# import itertools
+# floors = first, _, middle, _, _ = [1, 2, 3, 4, 5]  # interesting!
+# orderings = list(itertools.permutations(floors))
+# for (red, green, ivory, yellow, blue) in orderings:
+#     print (red, green, ivory, yellow, blue)
 
-import itertools
-floors = first, _, middle, _, _ = [1, 2, 3, 4, 5]  # interesting!
-orderings = list(itertools.permutations(floors))
-for (red, green, ivory, yellow, blue) in orderings:
-    print (red, green, ivory, yellow, blue)
+x  = set([1, 2, 3])
+z = sum(x)
+print z
+
+print set('theseletters')
