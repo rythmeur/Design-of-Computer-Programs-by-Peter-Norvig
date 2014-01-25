19c24506494cf410250373ca66fbd7954b3c3dfc
rythmeur <rythmeur@gmail.com>
2014-01-25 15:30:06 +0400

    25-01-2014

--- Test/123.py
+++ Test/123.py
@@ -9,15 +9,30 @@
 # for (red, green, ivory, yellow, blue) in orderings:
 #     print (red, green, ivory, yellow, blue)
 
-x  = set([1, 2, 3])
-z = sum(x)
-print z
+# x  = set([1, 2, 3])
+# z = sum(x)
+# print z
+#
+# print set('theseletters')
+#
+# z= "a b c d t f ew dsd fda"
+# x = z.split(" ", 2)
+# print x
+#
+# z,zz = ["aa", "bb"]
+# print z, zz
+#
+# z= [0, 1, 2]
+# print z
+# z.reverse()
+# print z
 
-print set('theseletters')
-
-z= "a b c d t f ew dsd fda"
-x = z.split(" ", 2)
-print x
-
-z,zz = ["aa", "bb"]
-print z, zz
+import re
+text = "Though I"
+# text = "<         b           > this <   /b    >, Though I"
+match_i = re.search("(<)(?: *)(.)",text)
+print type(match_i)
+if match_i:
+    print "Nontype"
+# match_i = match_i.groups()
+print match_i
--- hw3-3 Find Html Tags.py
+++ hw3-3 Find Html Tags.py
@@ -7,9 +7,42 @@
 # this problem.
 
 import re
+# My very stupid answer
+# def findtags(text):
+#     # your code here
+#     z=text.replace("<","SPLIT_MARGIN<")
+#     splited_text = z.split("SPLIT_MARGIN")
+#     splited_text2 = splited_text[:]
+#     indeces_to_del = []
+#     for i in range(len(splited_text)):
+#         match_i = re.search("(<)(?: *)(.)",splited_text[i])
+#         if match_i:
+#             match_i = match_i.groups()
+#             closing = False
+#             if len(match_i)>0:
+#                 # print "match_i", match_i, match_i[-1]
+#                 for j in range(i+1,len(splited_text)):
+#                     if "/" + match_i[-1] in splited_text[j]:
+#                         closing = True
+#             if not closing:
+#                 indeces_to_del.append(i)
+#     # print "splited_text", splited_text2
+#     indeces_to_del.reverse()
+#     for i in indeces_to_del:
+#         del splited_text2[i]
+#
+#     result = []
+#     # print "splited_text", splited_text2
+#     for string in splited_text2:
+#         if "/" not in string:
+#             result += re.findall('<.*?>',string)
+#     return result
 
+#Peter Norvig answer
 def findtags(text):
-    # your code here
+    parms = '(\w+\s*=\s*"[^"]*"\s*)*'
+    tags = '(<\s*\w+\s*' + parms + '\s*/?>)'
+    return re.findall(tags,text)
 
 
 testtext1 = """
@@ -42,3 +75,10 @@ def test():
     return 'tests pass'
 
 print test()
+print findtags(testtext1)
+print findtags(testtext2)
+print findtags(testtext3)
+
+# findtags(testtext1)
+# findtags(testtext2)
+# findtags(testtext3)
