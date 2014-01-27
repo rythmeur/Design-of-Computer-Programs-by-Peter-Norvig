64b623f32050bc39db5b0ad64fa517edb071e9c8
rythmeur <rythmeur@gmail.com>
2014-01-28 00:03:41 +0400

    28-01-2014

--- Lesson4.py
+++ Lesson4.py
@@ -134,7 +134,7 @@
 # tests for goal later: after pulling a state off the
 # frontier, not when we are about to put it on the
 # frontier.
-
+#
 # def bsuccessors(state):
 #     """Return a dict of {state:action} pairs.  A state is a (here, there, t) tuple,
 #     where here and there are frozensets of people (indicated by their times) and/or
@@ -192,63 +192,260 @@
 # print bridge_problem(frozenset((1, 2, 5, 10),))[-1][-1]
 # print  [elapsed_time(bridge_problem([1,1,2,3,5,8,13,21][:N])) for N in range(0)]
 
-# -----------------
-# User Instructions
+# # -----------------
+# # User Instructions
+# #
+# # Write a function, csuccessors, that takes a state (as defined below)
+# # as input and returns a dictionary of {state:action} pairs.
+# #
+# # A state is a tuple with six entries: (M1, C1, B1, M2, C2, B2), where
+# # M1 means 'number of missionaries on the left side.'
+# #
+# # An action is one of the following ten strings:
+# #
+# # 'MM->', 'MC->', 'CC->', 'M->', 'C->', '<-MM', '<-MC', '<-M', '<-C'
+# # where 'MM->' means two missionaries travel to the right side.
+# #
+# # We should generate successor states that include more cannibals than
+# # missionaries, but such a state should generate no successors.
 #
-# Write a function, csuccessors, that takes a state (as defined below)
-# as input and returns a dictionary of {state:action} pairs.
+# def csuccessors(state):
+#     """Find successors (including those that result in dining) to this
+#     state. But a state where the cannibals can dine has no successors."""
+#     M1, C1, B1, M2, C2, B2 = state
+#     actions_to_there = {'MM->':[2,0], 'MC->':[1,1], 'CC->':[0,2], 'M->':[1,0], 'C->':[0,1]}
+#     actions_from_there ={'<-MM':[2,0], '<-MC':[1,1],'<-CC':[0,2], '<-M':[1,0], '<-C':[0,1]}
+#     # your code here
+#     result ={}
+#     if M1<C1 or M2<C2: return {}
+#     if B1 !=0:
+#         # z = [(M1-m, C1-c) for m in range(M1+1) for c in range (C1+1) if (m +c <=3)]
+#         for a in actions_to_there:
+#             result[(M1-actions_to_there[a][0], C1-actions_to_there[a][1],0,M2+actions_to_there[a][0], C2+actions_to_there[a][1],1 )] = a
+#     if B2 !=0:
+#         for a in actions_from_there:
+#             result[(M1+actions_from_there[a][0], C1+actions_from_there[a][1],1,M2-actions_from_there[a][0], C2-actions_from_there[a][1],0 )] = a
 #
-# A state is a tuple with six entries: (M1, C1, B1, M2, C2, B2), where
-# M1 means 'number of missionaries on the left side.'
 #
-# An action is one of the following ten strings:
+#     return result
 #
-# 'MM->', 'MC->', 'CC->', 'M->', 'C->', '<-MM', '<-MC', '<-M', '<-C'
-# where 'MM->' means two missionaries travel to the right side.
+# def test():
+#     assert csuccessors((2, 2, 1, 0, 0, 0)) == {(2, 1, 0, 0, 1, 1): 'C->',
+#                                                (1, 2, 0, 1, 0, 1): 'M->',
+#                                                (0, 2, 0, 2, 0, 1): 'MM->',
+#                                                (1, 1, 0, 1, 1, 1): 'MC->',
+#                                                (2, 0, 0, 0, 2, 1): 'CC->'}
+#     assert csuccessors((1, 1, 0, 4, 3, 1)) == {(1, 2, 1, 4, 2, 0): '<-C',
+#                                                (2, 1, 1, 3, 3, 0): '<-M',
+#                                                (3, 1, 1, 2, 3, 0): '<-MM',
+#                                                (1, 3, 1, 4, 1, 0): '<-CC',
+#                                                (2, 2, 1, 3, 2, 0): '<-MC'}
+#     assert csuccessors((1, 4, 1, 2, 2, 0)) == {}
+#     return 'tests pass'
 #
-# We should generate successor states that include more cannibals than
-# missionaries, but such a state should generate no successors.
+# print test()
+
+# # -----------------
+# # User Instructions
+# #
+# # Write a function, shortest_path_search, that generalizes the search algorithm
+# # that we have been using. This function should have three inputs, a start state,
+# # a successors function, and an is_goal function.
+# #
+# # You can use the solution to mc_problem as a template for constructing your
+# # shortest_path_search. You can also see the example is_goal and successors
+# # functions for a simple test problem below.
+#
+# def shortest_path_search(start, successors, is_goal):
+#     """Find the shortest path from start state to a state
+#     such that is_goal(state) is true."""
+#     # your code here
+#     if is_goal(start): return start
+#     explored = set() # set of states we have visited
+#     frontier = [ [start] ] # ordered list of paths we have blazed
+#     while frontier:
+#         path = frontier.pop(0)
+#         print path
+#         s = path[-1]
+#
+#         for (state, action) in successors(s).items():
+#             if state not in explored:
+#                 explored.add(state)
+#                 path2 = path + [action, state]
+#                 if is_goal(state):
+#                     return path2
+#                 else:
+#                     frontier.append(path2)
+#                     print path2, "path2", "\n", frontier, "frontier"
+#     return Fail
+#
+# Fail = []
+#
+#
+# def mc_problem1(start=(3, 3, 1, 0, 0, 0), goal=None):
+#     """Solve the missionaries and cannibals problem.
+#     State is 6 ints: (M1, C1, B1, M2, C2, B2) on the start (1) and other (2) sides.
+#     Find a path that goes from the initial state to the goal state (which, if
+#     not specified, is the state with no people or boats on the start side."""
+#     if goal is None:
+#         goal = (0, 0, 0) + start[:3]
+#     if start == goal:
+#         return [start]
+#     explored = set() # set of states we have visited
+#     frontier = [ [start] ] # ordered list of paths we have blazed
+#     while frontier:
+#         path = frontier.pop(0)
+#         s = path[-1]
+#         for (state, action) in csuccessors(s).items():
+#             if state not in explored:
+#                 explored.add(state)
+#                 path2 = path + [action, state]
+#                 if state == goal:
+#                     return path2
+#                 else:
+#                     frontier.append(path2)
+#     return Fail
+#
+# Fail = []
 
 def csuccessors(state):
     """Find successors (including those that result in dining) to this
     state. But a state where the cannibals can dine has no successors."""
     M1, C1, B1, M2, C2, B2 = state
-    actions_to_there = {'MM->':[2,0], 'MC->':[1,1], 'CC->':[0,2], 'M->':[1,0], 'C->':[0,1]}
-    actions_from_there ={'<-MM':[2,0], '<-MC':[1,1],'<-CC':[0,2], '<-M':[1,0], '<-C':[0,1]}
-    # your code here
-    result ={}
-    if M1<C1 or M2<C2: return {}
-    if B1 !=0:
-        # z = [(M1-m, C1-c) for m in range(M1+1) for c in range (C1+1) if (m +c <=3)]
-        for a in actions_to_there:
-            result[(M1-actions_to_there[a][0], C1-actions_to_there[a][1],0,M2+actions_to_there[a][0], C2+actions_to_there[a][1],1 )] = a
-    if B2 !=0:
-        for a in actions_from_there:
-            result[(M1+actions_from_there[a][0], C1+actions_from_there[a][1],1,M2-actions_from_there[a][0], C2-actions_from_there[a][1],0 )] = a
+    ## Check for state with no successors
+    if C1 > M1 > 0 or C2 > M2 > 0:
+        return {}
+    items = []
+    if B1 > 0:
+        items += [(sub(state, delta), a + '->')
+                  for delta, a in deltas.items()]
+
+    if B2 > 0:
+        items += [(add(state, delta), '<-' + a)
+                  for delta, a in deltas.items()]
+    return dict(items)
+
+def add(X, Y):
+    "add two vectors, X and Y."
+    return tuple(x+y for x,y in zip(X, Y))
+
+def sub(X, Y):
+    "subtract vector Y from X."
+    return tuple(x-y for x,y in zip(X, Y))
+
+deltas = {(2, 0, 1,    -2,  0, -1): 'MM',
+          (0, 2, 1,     0, -2, -1): 'CC',
+          (1, 1, 1,    -1, -1, -1): 'MC',
+          (1, 0, 1,    -1,  0, -1): 'M',
+          (0, 1, 1,     0, -1, -1): 'C'}
+Fail = []
 
 
-    return result
+# --------------
+# Example problem
+#
+# Let's say the states in an optimization problem are given by integers.
+# From a state, i, the only possible successors are i+1 and i-1. Given
+# a starting integer, find the shortest path to the integer 8.
+#
+# This is an overly simple example of when we can use the
+# shortest_path_search function. We just need to define the appropriate
+# is_goal and successors functions.
 
+def is_goal(state):
+    if state == 8:
+        return True
+    else:
+        return False
 
+def successors(state):
+    successors = {state + 1: '->',
+                  state - 1: '<-'}
+    return successors
 
+#test
+assert shortest_path_search(5, successors, is_goal) == [5, '->', 6, '->', 7, '->', 8]
+print shortest_path_search(5, successors, is_goal)
 
+# -----------------
+# User Instructions
+#
+# Write a function, mc_problem2, that solves the missionary and cannibal
+# problem by making a call to shortest_path_search. Add any code below
+# and change the arguments in the return statement's call to the
+# shortest_path_search function.
 
+def mc_problem2(start=(3, 3, 1, 0, 0, 0), goal=None):
+    # your code here if necessary
+    return shortest_path_search() # <== insert arguments here
+
+def shortest_path_search(start, successors, is_goal):
+    """Find the shortest path from start state to a state
+    such that is_goal(state) is true."""
+    if is_goal(start):
+        return [start]
+    explored = set()
+    frontier = [ [start] ]
+    while frontier:
+        path = frontier.pop(0)
+        s = path[-1]
+        for (state, action) in successors(s).items():
+            if state not in explored:
+                explored.add(state)
+                path2 = path + [action, state]
+                if is_goal(state):
+                    return path2
+                else:
+                    frontier.append(path2)
+    return Fail
+Fail = []
+
+def csuccessors(state):
+    """Find successors (including those that result in dining) to this
+    state. But a state where the cannibals can dine has no successors."""
+    M1, C1, B1, M2, C2, B2 = state
+    ## Check for state with no successors
+    if C1 > M1 > 0 or C2 > M2 > 0:
+        return {}
+    items = []
+    if B1 > 0:
+        items += [(sub(state, delta), a + '->')
+                  for delta, a in deltas.items()]
+    if B2 > 0:
+        items += [(add(state, delta), '<-' + a)
+                  for delta, a in deltas.items()]
+    return dict(items)
+
+def add(X, Y):
+    "add two vectors, X and Y."
+    return tuple(x+y for x,y in zip(X, Y))
+
+def sub(X, Y):
+    "subtract vector Y from X."
+    return tuple(x-y for x,y in zip(X, Y))
+
+deltas = {(2, 0, 1,    -2,  0, -1): 'MM',
+          (0, 2, 1,     0, -2, -1): 'CC',
+          (1, 1, 1,    -1, -1, -1): 'MC',
+          (1, 0, 1,    -1,  0, -1): 'M',
+          (0, 1, 1,     0, -1, -1): 'C'}
 
 def test():
-    assert csuccessors((2, 2, 1, 0, 0, 0)) == {(2, 1, 0, 0, 1, 1): 'C->',
-                                               (1, 2, 0, 1, 0, 1): 'M->',
-                                               (0, 2, 0, 2, 0, 1): 'MM->',
-                                               (1, 1, 0, 1, 1, 1): 'MC->',
-                                               (2, 0, 0, 0, 2, 1): 'CC->'}
-    assert csuccessors((1, 1, 0, 4, 3, 1)) == {(1, 2, 1, 4, 2, 0): '<-C',
-                                               (2, 1, 1, 3, 3, 0): '<-M',
-                                               (3, 1, 1, 2, 3, 0): '<-MM',
-                                               (1, 3, 1, 4, 1, 0): '<-CC',
-                                               (2, 2, 1, 3, 2, 0): '<-MC'}
-    assert csuccessors((1, 4, 1, 2, 2, 0)) == {}
+    assert mc_problem2(start=(1, 1, 1, 0, 0, 0)) == [
+                             (1, 1, 1, 0, 0, 0), 'MC->',
+                             (0, 0, 0, 1, 1, 1)]
+    assert mc_problem2() == [(3, 3, 1, 0, 0, 0), 'CC->',
+                             (3, 1, 0, 0, 2, 1), '<-C',
+                             (3, 2, 1, 0, 1, 0), 'CC->',
+                             (3, 0, 0, 0, 3, 1), '<-C',
+                             (3, 1, 1, 0, 2, 0), 'MM->',
+                             (1, 1, 0, 2, 2, 1), '<-MC',
+                             (2, 2, 1, 1, 1, 0), 'MM->',
+                             (0, 2, 0, 3, 1, 1), '<-C',
+                             (0, 3, 1, 3, 0, 0), 'CC->',
+                             (0, 1, 0, 3, 2, 1), '<-C',
+                             (0, 2, 1, 3, 1, 0), 'CC->',
+                             (0, 0, 0, 3, 3, 1)]
     return 'tests pass'
 
 print test()
-
-
-
