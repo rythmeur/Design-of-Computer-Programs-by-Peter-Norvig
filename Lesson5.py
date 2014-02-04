044e7b69b2376262401ec77cd67cca64904c3d35
rythmeur <rythmeur@gmail.com>
2014-02-04 19:52:56 +0400

    04-02-2014

--- Lesson5.py
+++ Lesson5.py
@@ -53,74 +53,150 @@
 #
 # print test()
 
-# -----------------
-# User Instructions
+# # -----------------
+# # User Instructions
+# #
+# # Write a function, play_pig, that takes two strategy functions as input,
+# # plays a game of pig between the two strategies, and returns the winning
+# # strategy. Enter your code at line 41.
+# #
+# # You may want to borrow from the random module to help generate die rolls.
 #
-# Write a function, play_pig, that takes two strategy functions as input,
-# plays a game of pig between the two strategies, and returns the winning
-# strategy. Enter your code at line 41.
+# import random
 #
-# You may want to borrow from the random module to help generate die rolls.
+# possible_moves = ['roll', 'hold']
+# other = {1:0, 0:1}
+# goal = 50
+#
+# def clueless(state):
+#     "A strategy that ignores the state and chooses at random from possible moves."
+#     return random.choice(possible_moves)
+#
+# def hold(state):
+#     """Apply the hold action to a state to yield a new state:
+#     Reap the 'pending' points and it becomes the other player's turn."""
+#     (p, me, you, pending) = state
+#     return (other[p], you, me+pending, 0)
+#
+# def roll(state, d):
+#     """Apply the roll action to a state (and a die roll d) to yield a new state:
+#     If d is 1, get 1 point (losing any accumulated 'pending' points),
+#     and it is the other player's turn. If d > 1, add d to 'pending' points."""
+#     (p, me, you, pending) = state
+#     if d == 1:
+#         return (other[p], you, me+1, 0) # pig out; other player's turn
+#     else:
+#         return (p, me, you, pending+d)  # accumulate die roll in pending
+#
+# def play_pig(A, B):
+#     """Play a game of pig between two players, represented by their strategies.
+#     Each time through the main loop we ask the current player for one decision,
+#     which must be 'hold' or 'roll', and we update the state accordingly.
+#     When one player's score exceeds the goal, return that player."""
+#     # your code here
+#     strategies = [A,B]
+#     state = (0,0,0,0)
+#     while state[1] < goal and state[2] < goal:
+#         print state
+#         if strategies[state[0]](state) == "hold":
+#             state = hold(state)
+#         else:
+#             state = roll(state,random.choice([1,2,3,4,5,6]))
+#
+#     if state[0]==0:
+#         return B
+#     else:
+#         return A
+#
+#
+#
+# def always_roll(state):
+#     return 'roll'
+#
+# def always_hold(state):
+#     return 'hold'
+#
+# def test():
+#     for _ in range(10):
+#         winner = play_pig(always_hold, always_roll)
+#         assert winner.__name__ == 'always_roll'
+#     return 'tests pass'
+#
+# print test()
 
-import random
-
-possible_moves = ['roll', 'hold']
-other = {1:0, 0:1}
-goal = 50
-
-def clueless(state):
-    "A strategy that ignores the state and chooses at random from possible moves."
-    return random.choice(possible_moves)
-
-def hold(state):
-    """Apply the hold action to a state to yield a new state:
-    Reap the 'pending' points and it becomes the other player's turn."""
-    (p, me, you, pending) = state
-    return (other[p], you, me+pending, 0)
-
-def roll(state, d):
-    """Apply the roll action to a state (and a die roll d) to yield a new state:
-    If d is 1, get 1 point (losing any accumulated 'pending' points),
-    and it is the other player's turn. If d > 1, add d to 'pending' points."""
-    (p, me, you, pending) = state
-    if d == 1:
-        return (other[p], you, me+1, 0) # pig out; other player's turn
-    else:
-        return (p, me, you, pending+d)  # accumulate die roll in pending
-
-def play_pig(A, B):
-    """Play a game of pig between two players, represented by their strategies.
-    Each time through the main loop we ask the current player for one decision,
-    which must be 'hold' or 'roll', and we update the state accordingly.
-    When one player's score exceeds the goal, return that player."""
-    # your code here
-    strategies = [A,B]
-    state = (0,0,0,0)
-    while state[1] < goal and state[2] < goal:
-        print state
-        if strategies[state[0]](state) == "hold":
-            state = hold(state)
-        else:
-            state = roll(state,random.choice([1,2,3,4,5,6]))
-
-    if state[0]==0:
-        return B
-    else:
-        return A
 
 
+# # -----------------
+# # User Instructions
+# #
+# # Update the play_pig function so that it looks at the result
+# # that comes from the strategy function and makes sure that
+# # it is either 'hold' or 'roll' and if it's not one of those
+# # then that strategy should immediately lose and the other
+# # strategy should be declared the winner.
+#
+# import random
+#
+# def roll(state, d):
+#     """Apply the roll action to a state (and a die roll d) to yield a new state:
+#     If d is 1, get 1 point (losing any accumulated 'pending' points),
+#     and it is the other player's turn. If d > 1, add d to 'pending' points."""
+#     (p, me, you, pending) = state
+#     if d == 1:
+#         return (other[p], you, me+1, 0) # pig out; other player's turn
+#     else:
+#         return (p, me, you, pending+d)  # accumulate die roll in pending
+#
+# def hold(state):
+#     """Apply the hold action to a state to yield a new state:
+#     Reap the 'pending' points and it becomes the other player's turn."""
+#     (p, me, you, pending) = state
+#     return (other[p], you, me+pending, 0)
+#
+# def dierolls():
+#     "Generate die rolls."
+#     while True:
+#         yield random.randint(1, 6)
+#
+# other = {1:0, 0:1}
+# goal  = 40
+#
+# def play_pig(A, B, dierolls=dierolls()):
+#     """Play a game of pig between two players, represented by their strategies.
+#     Each time through the main loop we ask the current player for one decision,
+#     which must be 'hold' or 'roll', and we update the state accordingly.
+#     When one player's score exceeds the goal, return that player."""
+#     strategies = [A, B]
+#     state = (0, 0, 0, 0)
+#     while True:
+#         (p, me, you, pending) = state
+#         if me >= goal:
+#             return strategies[p]
+#         elif you >= goal:
+#             return strategies[other[p]]
+#         elif strategies[p](state) == 'hold':
+#             state = hold(state)
+#         elif strategies[p](state) == 'roll':
+#             state = roll(state, next(dierolls))
+#         else:
+#             return strategies[other[p]]
+#
+# def bad_strategy(state):
+#     "A strategy that could never win, unless a player makes an illegal move"
+#     return 'hold'
+#
+# def illegal_strategy(state):
+#     return 'I want to win pig please.'
+#
+# print play_pig(bad_strategy, illegal_strategy).__name__
+#
+# def test():
+#     winner = play_pig(bad_strategy, illegal_strategy)
+#     assert winner.__name__ == 'bad_strategy'
+#     return 'tests pass'
+#
+# print test()
 
-def always_roll(state):
-    return 'roll'
 
-def always_hold(state):
-    return 'hold'
 
-def test():
-    for _ in range(10):
-        winner = play_pig(always_hold, always_roll)
-        assert winner.__name__ == 'always_roll'
-    return 'tests pass'
-
-print test()
 
